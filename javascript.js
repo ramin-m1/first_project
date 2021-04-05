3a749b0b87c5d08fae0a6221481824632727ad4e
ramin-m1 <72311604+ramin-m1@users.noreply.github.com>
2021-04-05 11:32:15 -0400

    project initialized

---
+++ javascript.js
@@ -0,0 +1,7 @@
+verbal_bullying = "If you are being bullied verbally you should try to ignore them and not talk back or fight back. Things can only get worse if you react back."
+
+document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = verbal_bullying
+}
